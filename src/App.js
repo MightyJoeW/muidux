037e92a88688274ce38e8c350d6a2de1dc17af76
Joe Warren <joe.warren7@gmail.com>
2018-05-27 14:28:58 -0500

    style: replace external stylesheets with inline styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
+// External Dependencies
 import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import logo from './logo.svg';
 import './App.css';
 
+// Local Variables
+const app = {
+  textAlign: 'center',
+  fontFamily: 'sans-serif',
+}
+
+const appHeader = {
+  backgroundColor: '#222',
+  height: '150px',
+  padding: '20px',
+  color: 'white',
+}
+
+const appLogo = {
+  animation: 'App-logo-spin infinite 20s linear',
+  height: '80px',
+}
+
+const appTitle = {
+  fontSize: '1.5em',
+}
+
+const appIntro = {
+  fontSize: 'large',
+}
+
+// Component Definition
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div style={app}>
+        <CssBaseline />
+        <header style={appHeader}>
+          <img src={logo} style={appLogo} alt="logo" />
+          <h1 style={appTitle}>Welcome to MuiDucks</h1>
         </header>
-        <p className="App-intro">
+        <p style={appIntro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
